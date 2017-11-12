5166e8cce0745c9e356efc24c5b6a11e0af775d2
kazu-mi <kazuyuka76@gmail.com>
2017-11-12 21:40:49 +0900

    add modules

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
+        loaders: ['style-loader', 'css-loader?modules'],
         include: [APP, NODE_MODULES],
       },
       {
